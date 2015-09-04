6f44c558cc05d5f50943e5685fec86c116708f03
JumpingJack200 <pascalkaakschlief@web.de>
2015-09-04 10:52:43 +0200

    Test

---
+++ ruby/code/studio_game.rb
@@ -0,0 +1,14 @@
+name1 = "larry"
+health = 60
+puts "#{name1}'s health is #{health}"
+puts name1 + "'s health is " + health.to_s
+puts name1 + "'s health is #{3*health}"
+puts name1 + "'s health is #{health/9}"
+puts name1 + "'s health is #{health/9.0}"
+puts "Players: \n\tLarry\n\tCurly\n\tmoe"
+name2= "Curly"
+name3= "moe"
+puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
+
+
+
