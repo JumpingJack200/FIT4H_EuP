132c58964df81c84126053bbf0bf3f70ff6fb320
JumpingJack200 <pascalkaakschlief@web.de>
2015-09-24 09:53:56 +0200

    Updatesvonheute

--- ruby/code/flicks.rb
+++ ruby/code/flicks.rb
@@ -1,5 +1,25 @@
-class Movie
+class Playlist
+	def initialize(name)
+		@name = name
+		@movies = []
+	end
 
+	def add_movie(movie)
+		@movies << movie
+	end
+	def to_s
+		@movies.size.to_s
+	end	
+end
+movie1 = Movie.new('NinjaTurtles', 4)
+movie2 = Movie.new('45 years',8)
+movie3 = Movie.new('8 mm',10)
+pl = Playlist.new("Horror")
+pl.add_movie(movie1)
+puts pl
+
+
+class Movie
 	attr_accessor :title
 	def initialize(ptitle, prank=0)
 		@title = ptitle.capitalize
@@ -15,41 +35,3 @@ class Movie
 		"#{@title} hat ein Ranking von: #{@rank}."
 	end
 end
-
-movie1 = Movie.new('NinjaTurtles', 4)
-movie2 = Movie.new('45 years',8)
-movie3 = Movie.new('8 mm',10)
-
-movies = [movie1, movie2, movie3]
-movies.each do |movie|
-	movie.thumbs_up
-	movie.thumbs_down
-	puts movie
-end
-
-
-
-
-# def movie_listing(title,rank=10)
-# 	"Movie: #{title.downcase.capitalize} hat #{weekday} das Ranking #{rank}"
-# end
-
-# def weekday
-#   Time.now.strftime("%A")
-# end
-
-
-# puts movie_listing("Ninja Turtles",4)	
-# puts movie_listing("SWAT",3)
-
-# Beispiel für Variablen innerhalb von Methoden
-# def movie_listing
-# 	var = "Movie:Ninja Turtles"
-# 	puts "var innerhalb Methode: #{var.object_id}"
-# 	var2 = "Movie: irgendein Film"
-# 	"#{var}\n#{var2}"
-# end
-
-# var = movie_listing	
-# puts "var ausseralb: #{var.object_id}"
-# puts "Rückgabewert #{var}"
